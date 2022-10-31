13d8192d43a7eda19085c60e3fa2718eadcfcab1
User <User@DESKTOP-ESP2JT1>
2022-10-31 16:43:47 +0200

    Add project files.

---
+++ 07. Projects Creation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _07._Projects_Creation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string architect = Console.ReadLine();
+            int projects = int.Parse(Console.ReadLine());
+            int hours = projects *3;
+            Console.WriteLine($"The architect {architect} will need {hours} hours to complete {projects} project/s.");
+
+
+
+        }
+    }
+}
